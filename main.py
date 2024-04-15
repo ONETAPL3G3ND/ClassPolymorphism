6c0f25bb48a2f594dc4d5be4e31eb0cb974225bc
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-04-15 06:48:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from functools import singledispatchmethod
+
+
+class Petrol95:
+    def __init__(self):
+        ...
+
+class Diesel:
+    def __init__(self):
+        ...
+
+class Car:
+    def __init__(self, name):
+        self.Name = name
+        self.Fuel = 0
+
+    def StartEngine(self):
+        if self.Fuel != 0:
+            print("Engine started")
+            return
+        print("low gasoline level")
+
+    @singledispatchmethod
+    def FillCar(self, fuel):
+        print("This fuel is not suitable")
+
+    @FillCar.register
+    def _(self, fuel: Petrol95):
+        self.Fuel = 100
+        print("The car is refueled")
+    @FillCar.register
+    def _(self, fuel: Diesel):
+        self.Fuel = 100
+        print("The engine is refueled, but does not work stably")
+
+
+car = Car("Tesla")
+car.StartEngine()
+
+petrol = Petrol95()
+diesel = Diesel()
+
+car.FillCar("Other fuel")
+car.FillCar(petrol)
+car.FillCar(diesel)
+
+car.StartEngine()
+
+
+
+
